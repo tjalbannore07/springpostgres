d330d729caa7b1dd737e9b6765cad501d83f1afa
tjalbannore07 <tejal.bannore07@gmail.com>
2024-01-13 12:13:04 +0530

    Code Commit

---
+++ src/test/java/com/example/springbootwithpostgresqldevelopment/SpringBootWithPostgreSqlDevelopmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootwithpostgresqldevelopment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWithPostgreSqlDevelopmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
